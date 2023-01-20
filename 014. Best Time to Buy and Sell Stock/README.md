523892393882ca143ca74acc3e1e12094366ec6d
Kutoriushio <328752344@qq.com>
2023-01-19 21:10:51 -0500

    solution for 014

---
+++ 014. Best Time to Buy and Sell Stock/README.md
@@ -0,0 +1,3 @@
+# Solution(Python)
+
+For every element, we are calculating the difference between that element and the minimum of all the values before that element and we are updating the maximum profit if the difference thus found is greater than the current maximum profit.
---
+++ 014. Best Time to Buy and Sell Stock/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit= 0
+        min_price = 10**8
+        for price in prices:
+            profit = price - min_price
+            if profit > max_profit:
+                max_profit = profit
+            if price < min_price:
+                     = price
+        
+        return max_profit
