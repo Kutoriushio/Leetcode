0bfc00df0d26601faf5670caaac81b325ca42249
Kutoriushio <328752344@qq.com>
2023-01-24 21:21:21 -0500

    solution for 032

---
+++ 032. Pascal's Triangle/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = []
+        for i in range(1, numRows+1):
+            temp = []
+            for j in range(i):
+                if j == 0:
+                    temp.append(1)
+                elif j == i - 1:
+                    temp.append(1)
+                else:
+                    temp.append(res[-1][j-1] + res[-1][j])
+            res.append(temp)
+        return res
