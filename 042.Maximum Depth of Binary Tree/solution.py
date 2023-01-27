958cd4b2519be109855dd1352372ad8ceb68ee21
Kutoriushio <328752344@qq.com>
2023-01-27 18:19:02 -0500

    "solution for 042"

---
+++ 042.Maximum Depth of Binary Tree/solution.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root == None:
+            return 0
+        queue = [root]
+        res = []
+        while queue:
+            cur_level = []
+            next_level = []
+            for node in queue:
+                cur_level.append(node)
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            res.append(cur_level)
+            queue = next_level
+        
+        return len(res)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            if node == None:
+                return 0
+            left = dfs(node.left)
+            right = dfs(node.right)
+            return max(left,right)+1
+        return dfs(root)
