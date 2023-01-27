7361b505078e6dc1f8d93497aafde7fd9b2f6cd4
Kutoriushio <328752344@qq.com>
2023-01-27 18:22:09 -0500

    solution for 042

--- 042.Maximum Depth of Binary Tree/solution.py
+++ 042.Maximum Depth of Binary Tree/solution.py
@@ -37,5 +37,5 @@ class Solution:
                 return 0
             left = dfs(node.left)
             right = dfs(node.right)
-            return max(left,right)+1
+            return max(left,right)+1 #depth + 1
         return dfs(root)
