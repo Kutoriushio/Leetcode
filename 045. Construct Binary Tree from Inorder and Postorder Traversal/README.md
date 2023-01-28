8d9327c26b558e7c79fa8b165bed30598279d3b7
Kutoriushio <328752344@qq.com>
2023-01-28 15:56:53 -0500

    update solution for 045

--- 045. Construct Binary Tree from Inorder and Postorder Traversal/README.md
+++ 045. Construct Binary Tree from Inorder and Postorder Traversal/README.md
@@ -1,7 +1,20 @@
 # Solution(Python)
 
+## Recrusive
 1.  If `left>right`,  the subtree is empty and the empty node is returned.
 
 2. Select the last node of the post-order traversal as the root node.
 
 3. Based on post-order traversal logic, recursive right subtree creation and left subtree creation.
+
+## Iterative
+
+1. Initially the stack holds the root node (the last node of the postorder traversal), with a pointer to the last node of the inorder traversal.
+
+2. Enumerate each node in the forward traversal in turn, except for the last node.
+
+-- If index happens to point to the top of the stack, then we keep popping the top node and moving index to the left, and use the current node as the left child of the last node popped.
+
+-- if index is different from the top of the stack node, we treat the current node as the right child of the top of the stack node.
+
+3. Add the current node to the stack.
--- 045. Construct Binary Tree from Inorder and Postorder Traversal/solution.py
+++ 045. Construct Binary Tree from Inorder and Postorder Traversal/solution.py
@@ -20,3 +20,28 @@ class Solution:
         for i, v in enumerate(inorder):
             dic[v] = i
         return helper(0, len(inorder)-1)
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not postorder:
+            return None
+
+        n = len(postorder)-1
+        val = postorder[n]
+        root = TreeNode(val)
+        stack = [root]
+        index = n
+        for i in range(1, n+1):
+            node = stack[-1]
+            val = postorder[n-i]
+            if node.val != inorder[index]:
+                node.right = TreeNode(val)
+                stack.append(node.right)
+            else:
+                while stack and stack[-1].val == inorder[index]:
+                    node = stack.pop()
+                    index -= 1
+                node.left = TreeNode(val)
+                stack.append(node.left)
+        
+        return root
