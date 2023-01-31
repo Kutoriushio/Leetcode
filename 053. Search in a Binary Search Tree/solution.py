330e3335e9ed932cd67fe017753fdadc09d1098b
Kutoriushio <328752344@qq.com>
2023-01-31 16:14:55 -0500

    solution for 053

---
+++ 053. Search in a Binary Search Tree/solution.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Recrusive (TC(O(N)) SC(O(N)))
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return
+        if root.val == val:
+            return root
+        if root.val > val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
+
+# Iterative (TC(O(N)) SC(O(1)))
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        while root:
+            if root.val == val:
+                return root
+            if root.val > val:
+                root = root.left
+            else:
+                root = root.right
+        
+        return None
